c2946ee828cbfa83cd660b2b8531f162fb83cef5
halamhung <hungdeptrai111@gmail.com>
2024-09-18 11:24:43 +0700

    update by hung 18/9

---
+++ src/main/java/com/example/ecommerce/Backend/IService/IProductServices.java
@@ -0,0 +1,4 @@
+package com.example.ecommerce.Backend.IService;
+
+public class IProductServices {
+}
--- src/main/java/com/example/ecommerce/Backend/ServiceImpl/ProductServicesImpl.java
+++
@@ -1,4 +0,0 @@
-package com.example.ecommerce.Backend.ServiceImpl;
-
-public class ProductServicesImpl {
-}

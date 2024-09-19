fe0c75a9a0f1ddc813ab1817e4b1f9f1b025e665
halamhung <hungdeptrai111@gmail.com>
2024-09-19 16:47:23 +0700

    hung 19/9 create, get product

--- src/main/java/com/example/ecommerce/Backend/Responses/BaseResponse.java
+++ src/main/java/com/example/ecommerce/Backend/Responses/BaseResponse.java
@@ -1,13 +1,16 @@
 package com.example.ecommerce.Backend.Responses;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
-
+@Data
 public class BaseResponse {
     @JsonProperty("created_at")
-    private Date createdAt;
+    private LocalDate createdAt;
     @JsonProperty("updated_at")
-    private Date updatedAt;
+    private LocalDate updatedAt;
 }
